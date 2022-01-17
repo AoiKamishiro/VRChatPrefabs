66aa7457341ad5ba4536ba1e9122d050007c93f0
AoiKamishiro <vr.aoi.kamishiro@gmail.com>
2022-01-17 10:27:37 +0900

    Update Packages

--- Assets/Varneon/WorldCreatorAssistant/Editor/EditorPreferenceKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Varneon.WorldCreatorAssistant
-{
-    internal static class EditorPreferenceKeys
-    {
-        internal const string LastVRCAPIRequest = "Varneon/WCA/LastVRChatAPIRequest";
-        internal const string PackageCache = "Varneon/WCA/PackageCacheDirectory";
-        internal const string Language = "Varneon/WCA/Language";
-    }
-}
